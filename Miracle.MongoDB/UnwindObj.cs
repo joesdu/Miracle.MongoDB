b04dc68ab9fb01ec19602ff55f4da1b9a7958a1b
Joes <dygood@outlook.com>
2021-12-07 19:12:35 +0800

    添加注释

--- Miracle.MongoDB/UnwindObj.cs
+++ Miracle.MongoDB/UnwindObj.cs
@@ -3,10 +3,20 @@
 namespace Miracle.MongoDB;
 public class UnwindObj<T>
 {
+    /// <summary>
+    /// 1.T as List<T>,use for Projection,
+    /// 2.T as single Object,use for MongoDB array field Unwind result
+    /// </summary>
     [BsonElement("Obj")]
-    public T? Obj { get; set; }//1.T as List<T>,use for Projection,2.T as single Object,use for MongoDB array field Unwind result
+    public T? Obj { get; set; }
+    /// <summary>
+    /// when T as List<T>,record Count
+    /// </summary>
     [BsonElement("Count")]
-    public int Count { get; set; }//when T as List<T>,record Count
+    public int Count { get; set; }
+    /// <summary>
+    /// record array field element's index before Unwindss
+    /// </summary>
     [BsonElement("Index")]
-    public int Index { get; set; }//record array field element's index before Unwindss
+    public int Index { get; set; }
 }
